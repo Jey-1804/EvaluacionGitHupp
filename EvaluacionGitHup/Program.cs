ad7d9fdabd0cc2632ffeca945c8b20f55084409d
Jey-1818 <jeymih757@gmail.com>
2025-05-13 15:03:43 -0600

    Agregado el metodo para mostrar mis datos

--- EvaluacionGitHup/Program.cs
+++ EvaluacionGitHup/Program.cs
@@ -2,9 +2,16 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public void Main(string[] args)
         {
             Console.WriteLine("Proyecto de ventas");
+            MostrarDatos();
         }
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Nombre: Jamie Maryeli Herrera Herrera");
+            Console.WriteLine("Carnet: 24510609");
+        }
+
     }
 }
